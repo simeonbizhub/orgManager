e20f0a5cd6fd15209d2207737a034e5288a59fa0
Simeon Solomou <simeon.solomou@bizhub.com.au>
2023-08-03 16:49:58 +0930

    Init Commit

---
+++ src/generated/java/modules/orgManagement/domain/Office.java
@@ -0,0 +1,253 @@
+package modules.orgManagement.domain;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import org.skyve.CORE;
+import org.skyve.domain.messages.DomainException;
+import org.skyve.impl.domain.AbstractPersistentBean;
+
+/**
+ * Office
+ * 
+ * @stereotype "persistent"
+ */
+@XmlType
+@XmlRootElement
+public class Office extends AbstractPersistentBean {
+	/**
+	 * For Serialization
+	 * @hidden
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** @hidden */
+	public static final String MODULE_NAME = "orgManagement";
+
+	/** @hidden */
+	public static final String DOCUMENT_NAME = "Office";
+
+	/** @hidden */
+	public static final String levelUnitPropertyName = "levelUnit";
+
+	/** @hidden */
+	public static final String buildingNamePropertyName = "buildingName";
+
+	/** @hidden */
+	public static final String streetAddressPropertyName = "streetAddress";
+
+	/** @hidden */
+	public static final String suburbPropertyName = "suburb";
+
+	/** @hidden */
+	public static final String postCodePropertyName = "postCode";
+
+	/** @hidden */
+	public static final String phonePropertyName = "phone";
+
+	/** @hidden */
+	public static final String imagePropertyName = "image";
+
+	/**
+	 * Level Unit
+	 **/
+	private String levelUnit;
+
+	/**
+	 * Building Name
+	 **/
+	private String buildingName;
+
+	/**
+	 * Street Address
+	 **/
+	private String streetAddress;
+
+	/**
+	 * Suburb
+	 **/
+	private String suburb;
+
+	/**
+	 * PostCode
+	 **/
+	private String postCode;
+
+	/**
+	 * Phone
+	 **/
+	private String phone;
+
+	/**
+	 * Image
+	 **/
+	private String image;
+
+	@Override
+	@XmlTransient
+	public String getBizModule() {
+		return Office.MODULE_NAME;
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizDocument() {
+		return Office.DOCUMENT_NAME;
+	}
+
+	public static Office newInstance() {
+		try {
+			return CORE.getUser().getCustomer().getModule(MODULE_NAME).getDocument(CORE.getUser().getCustomer(), DOCUMENT_NAME).newInstance(CORE.getUser());
+		}
+		catch (RuntimeException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new DomainException(e);
+		}
+	}
+
+	@Override
+	@XmlTransient
+	public String getBizKey() {
+		try {
+			return org.skyve.util.Binder.formatMessage("Office - {levelUnit}", this);
+		}
+		catch (@SuppressWarnings("unused") Exception e) {
+			return "Unknown";
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return ((o instanceof Office) && 
+					this.getBizId().equals(((Office) o).getBizId()));
+	}
+
+	/**
+	 * {@link #levelUnit} accessor.
+	 * @return	The value.
+	 **/
+	public String getLevelUnit() {
+		return levelUnit;
+	}
+
+	/**
+	 * {@link #levelUnit} mutator.
+	 * @param levelUnit	The new value.
+	 **/
+	@XmlElement
+	public void setLevelUnit(String levelUnit) {
+		preset(levelUnitPropertyName, levelUnit);
+		this.levelUnit = levelUnit;
+	}
+
+	/**
+	 * {@link #buildingName} accessor.
+	 * @return	The value.
+	 **/
+	public String getBuildingName() {
+		return buildingName;
+	}
+
+	/**
+	 * {@link #buildingName} mutator.
+	 * @param buildingName	The new value.
+	 **/
+	@XmlElement
+	public void setBuildingName(String buildingName) {
+		preset(buildingNamePropertyName, buildingName);
+		this.buildingName = buildingName;
+	}
+
+	/**
+	 * {@link #streetAddress} accessor.
+	 * @return	The value.
+	 **/
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	/**
+	 * {@link #streetAddress} mutator.
+	 * @param streetAddress	The new value.
+	 **/
+	@XmlElement
+	public void setStreetAddress(String streetAddress) {
+		preset(streetAddressPropertyName, streetAddress);
+		this.streetAddress = streetAddress;
+	}
+
+	/**
+	 * {@link #suburb} accessor.
+	 * @return	The value.
+	 **/
+	public String getSuburb() {
+		return suburb;
+	}
+
+	/**
+	 * {@link #suburb} mutator.
+	 * @param suburb	The new value.
+	 **/
+	@XmlElement
+	public void setSuburb(String suburb) {
+		preset(suburbPropertyName, suburb);
+		this.suburb = suburb;
+	}
+
+	/**
+	 * {@link #postCode} accessor.
+	 * @return	The value.
+	 **/
+	public String getPostCode() {
+		return postCode;
+	}
+
+	/**
+	 * {@link #postCode} mutator.
+	 * @param postCode	The new value.
+	 **/
+	@XmlElement
+	public void setPostCode(String postCode) {
+		preset(postCodePropertyName, postCode);
+		this.postCode = postCode;
+	}
+
+	/**
+	 * {@link #phone} accessor.
+	 * @return	The value.
+	 **/
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * {@link #phone} mutator.
+	 * @param phone	The new value.
+	 **/
+	@XmlElement
+	public void setPhone(String phone) {
+		preset(phonePropertyName, phone);
+		this.phone = phone;
+	}
+
+	/**
+	 * {@link #image} accessor.
+	 * @return	The value.
+	 **/
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * {@link #image} mutator.
+	 * @param image	The new value.
+	 **/
+	@XmlElement
+	public void setImage(String image) {
+		preset(imagePropertyName, image);
+		this.image = image;
+	}
+}
